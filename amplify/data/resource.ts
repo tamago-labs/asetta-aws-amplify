556b08ea0c423fe74edf38a24b59c84a99a9319f
pisuthd <pisuth.dae@gmail.com>
2025-06-24 10:10:14 +0900

    Fix syntax error

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,11 +1,11 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
- 
+
 const schema = a.schema({
   Chat: a.conversation({
     aiModel: a.ai.model("Claude 3.5 Sonnet"),
     systemPrompt: 'You are a helpful assistant',
   })
-    .authorization((allow) => [allow.owner()])
+    .authorization((allow) => allow.owner())
 });
 
 export type Schema = ClientSchema<typeof schema>;
